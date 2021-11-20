84249848f17d14c80d433bc079b6b1213119b36a
Hgjnnf <gordonhwc1@gmail.com>
2021-11-20 14:35:50 -0800

    Datapath modification - stage 4.1

--- datapath.v
+++ datapath.v
@@ -1,4 +1,4 @@
-module datapath(clk,readnum,vsel,loada,loadb,shift,asel,bsel,ALUop,loadc,loads,writenum,write,datapath_in,Z_out,datapath_out);
+module datapath(clk,readnum,vsel,loada,loadb,shift,asel,bsel,ALUop,loadc,loads,writenum,write,mdata,PC,sximm5,sximm8,Z_out,datapath_out);
   //clk input
   input clk;
   //read,write addresses
@@ -6,13 +6,15 @@ module datapath(clk,readnum,vsel,loada,loadb,shift,asel,bsel,ALUop,loadc,loads,w
   //operation selecters
   input [1:0] ALUop, shift;
   //selecter and load enable signals
-  input write, vsel, loada, loadb, asel, bsel, loadc, loads;
+  input write, loada, loadb, asel, bsel, loadc, loads;
+  input [1:0] vsel;
   //data input
-  input [15:0] datapath_in; 
+  input [15:0] sximm5, sximm8, mdata;
+  input [7:0] PC;
 
   //outputs
   output [15:0] datapath_out;
-  output Z_out;
+  output [2:0] Z_out;
 
   //instantiation variables
   wire [15:0] data_out;
@@ -22,7 +24,7 @@ module datapath(clk,readnum,vsel,loada,loadb,shift,asel,bsel,ALUop,loadc,loads,w
   wire [15:0] in;
   wire [15:0] in_loadA;
   wire [15:0] datapath_out;
-  wire Z_out;
+  wire [2:0] Z_out;
 
   //additional mux variables
   reg [15:0] data_in;
@@ -35,11 +37,14 @@ module datapath(clk,readnum,vsel,loada,loadb,shift,asel,bsel,ALUop,loadc,loads,w
   shifter U1(.in(in),.shift(shift),.sout(sout));
 
   //additional muxes
-  always@(*)begin  //writeback mux
-    if(vsel == 1'b1)
-      data_in = datapath_in;
-    else
-      data_in = datapath_out;
+  always@(*)begin  //regfile input mux
+    case(vsel)
+      2'b00: data_in = datapath_out;
+      2'b01: data_in = {8'b0, PC};
+      2'b10: data_in = sximm8;
+      2'b11: data_in = mdata;
+      default: data_in = 16'b0;
+    endcase
   end
 
   always@(*)begin
@@ -51,7 +56,7 @@ module datapath(clk,readnum,vsel,loada,loadb,shift,asel,bsel,ALUop,loadc,loads,w
 
   always@(*)begin
     if(bsel == 1'b1) //bsel for Bin
-      Bin = {11'b00000000000,datapath_in[4:0]};
+      Bin = sximm5;
     else
       Bin = sout;
   end
@@ -60,7 +65,7 @@ module datapath(clk,readnum,vsel,loada,loadb,shift,asel,bsel,ALUop,loadc,loads,w
   vDFFE #(16) A(clk, loada, data_out, in_loadA);
   vDFFE #(16) B(clk, loadb, data_out, in);
   vDFFE #(16) C(clk, loadc, out, datapath_out);
-  vDFFE #(1) STATUS(clk, loads, Z, Z_out);
+  vDFFE #(3) STATUS(clk, loads, {(Ain[15]^Bin[15]) ? 0 : (out[15]^Ain[15]), out[15], Z}, Z_out);
   
 endmodule
   
